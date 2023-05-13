7edc5c5c5175626a7d26b9e00fff29673bc249e4
Anton Buketov <38258424+KotBegemotEST@users.noreply.github.com>
2023-05-13 21:47:10 +0300

    security update

--- server/config.js
+++ server/config.js
@@ -1,10 +1,4 @@
-// var databaseOptions = {
-//     host: 'wasat.elkdata.ee',
-//     user: "vhost85181s2",
-//     password: "JDXHUs5SW6nK8nReVhmaCAHG3TZdQPu9",
-//     database: "vhost85181s2",
-// }
-// module.exports = {databaseOptions: databaseOptions} ;
+
 
 
 
